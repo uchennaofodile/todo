7a87e280264d5639f0765ca5bc4953016348de38
Uchenna <uchenna.ofodile@gmail.com>
2023-03-22 23:10:21 -0400

    Added start

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 
-
+app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 //allows us to access 
